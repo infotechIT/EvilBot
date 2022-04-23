23d85476168eba2c3b4f4b92a1a57dfe80debe09
InfoTech <infotechtele@gmail.com>
2022-04-23 20:47:34 +0530

    Update sample_config.py

--- EvilBot/sample_config.py
+++ EvilBot/sample_config.py
@@ -17,8 +17,8 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1742353529  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "shrvan42"
+    OWNER_ID = 1827034171  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "alxneboy"
     SUPPORT_CHAT = "EVIL_SUPPORT_CHAT"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001236063876
